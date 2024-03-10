849819ce14e707926883b2c0a4a9cc38bdbe59a9
ahinsa-1111 <148263893+ahinsa-1111@users.noreply.github.com>
2024-03-10 22:38:26 +0530

    commit_04

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -5,6 +5,28 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Color(0xFF232227),
+      body: SafeArea(
+        child: Padding(
+          padding: EdgeInsets.only(top: 25),
+          child: Column(
+            children: [
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: 15),
+                child: Row(children: [
+                  InkWell(
+                    onTap: () {},
+                    child: Icon(
+                      Icons.sort_rounded,
+                      color: Colors.white,
+                      size: 35,
+                    ),
+                  ),
+                ]),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
