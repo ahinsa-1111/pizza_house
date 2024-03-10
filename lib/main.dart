00bb5c2ba7356d9d33d35cb80aa029054ba7373c
ahinsa-1111 <148263893+ahinsa-1111@users.noreply.github.com>
2024-03-10 22:17:36 +0530

    commit_03

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:pizza_house/home_page.dart';
+import 'package:flutter/services.dart';
 
 void main() {
   runApp(myApp());
@@ -11,9 +13,18 @@ class myApp extends StatefulWidget {
 
 class _MyAppState extends State<myApp> {
   @override
+  void initState() {
+    SystemChrome.setEnabledSystemUIMode(SystemUiMode.immersiveSticky);
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
+      routes: {
+        '/': (context) => HomePage(),
+      },
     );
   }
 }
