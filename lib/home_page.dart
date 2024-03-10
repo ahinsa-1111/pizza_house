4f3957f81a89e347b67723e135277c7c8b3b3261
ahinsa-1111 <148263893+ahinsa-1111@users.noreply.github.com>
2024-03-10 23:05:59 +0530

    commit_05

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -9,20 +9,41 @@ class HomePage extends StatelessWidget {
         child: Padding(
           padding: EdgeInsets.only(top: 25),
           child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               Padding(
                 padding: EdgeInsets.symmetric(horizontal: 15),
-                child: Row(children: [
-                  InkWell(
-                    onTap: () {},
-                    child: Icon(
-                      Icons.sort_rounded,
-                      color: Colors.white,
-                      size: 35,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    InkWell(
+                      onTap: () {},
+                      child: Icon(
+                        Icons.sort_rounded,
+                        color: Colors.white,
+                        size: 35,
+                      ),
                     ),
-                  ),
-                ]),
+                    InkWell(
+                      onTap: () {},
+                      child: Icon(
+                        Icons.search,
+                        color: Colors.white,
+                        size: 35,
+                      ),
+                    ),
+                  ],
+                ),
               ),
+              SizedBox(height: 30),
+              Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 15),
+                  child: Text("Hot & Fast Food",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 32,
+                        fontWeight: FontWeight.bold,
+                      )))
             ],
           ),
         ),
